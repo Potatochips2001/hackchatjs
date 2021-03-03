c5fa7ed98822279152c5ccea2ae94042e196ef4f
Potatochips2001 <76017734+Potatochips2001@users.noreply.github.com>
2021-03-02 23:52:05 -0500

    hackchatjs

--- hackchat.js
+++ hackchat.js
@@ -6,17 +6,30 @@ var ignored = [];
 var afkList = [];
 let botUptime = 0.0;
 var randomColors = false;
+var getDataRec = "";
 
 sendJoin = {
     "cmd": "join",
-    "channel": "CHANNEL",
-    "nick": "BOTS_NAME#PASSWORD"
+    "channel": "programming",
+    "nick": "nick"
 }
 
 const { Console } = require('console');
 const { stringify } = require('querystring');
 const WebSocket = require('ws');
 const ws = new WebSocket('wss://hack.chat/chat-ws');
+const httpGet = require('https');
+
+function sendGet(uri) {
+    httpGet.get(uri, (myResponse) => {
+        myResponse.on('data', (recData) => {
+            getDataRec += recData;
+        });
+    }).on('error', (e) => {
+        console.log(e);
+        getDataRec = null;
+    });
+}
 
 function send(data) {
     if (ws && ws.readyState == ws.OPEN) {
@@ -31,20 +44,29 @@ let firstPacket = true;
 setInterval(function () {
     botUptime++;
 }, 1000);
+
+let packetRecTime = null;
+let lastMessage = "";
+
 ws.on('message', function incoming(event) {
-    msgData = JSON.parse(event);
-    msgR = String(msgData.text);
-    msgNick = String(msgData.nick);
-    msgTrip = String(msgData.trip);
-    msgFrom = String(msgData.from);
-    msgCmd = String(msgData.cmd);
+    msgData = JSON.parse(event); //Just the data that was recieved
+    msgR = String(msgData.text); //Message
+    msgNick = String(msgData.nick); //Who sent the message (use for chat)
+    msgTrip = String(msgData.trip); //Trip of user
+    msgFrom = String(msgData.from); //Who sent the message (use for whisper)
+    msgCmd = String(msgData.cmd); //Command
     if (firstPacket == true) {
         usersOnline = String(msgData.nicks);
         firstPacket = false;
     }
+    //Check the most recent message
+    if (msgCmd == "chat" && !msgR.startsWith("./lastactive")) {
+        packetRecTime = Date.now();
+        lastMessage = msgNick + ": " + msgR;
+    }
     //Help
     if (msgR == "./help" && !ignored.includes(msgNick)) {
-        send({"cmd": "chat", "text": "Commands: ./color, ./coloron, ./coloroff, ./random, ./afk, ./uptime\nAdmin: ./ignore, ./accept, ./showafk"})
+        send({"cmd": "chat", "text": "$\\red{https://github.com/Potatochips2001/hackchatjs}$\nCommands: ./color, ./coloron, ./coloroff, ./random, ./afk, ./uptime, ./printcolor, ./channel, ./lastactive\nAdmin: ./ignore, ./accept, ./showafk, ./showignored"})
     }
     //Colors
     if (msgR.startsWith("./color") && !ignored.includes(msgNick)) {
@@ -74,7 +96,7 @@ ws.on('message', function incoming(event) {
         send({ "cmd": "chat", "text": msgNick + " is no longer AFK" });
     }
     //Go AFK
-    if (msgR.startsWith("./afk")) {
+    if (msgR.startsWith("./afk") && !msgR.includes("./afklist")) {
         afkList.push(msgNick);
         send({ "cmd": "chat", "text": msgNick + " is AFK" });
     }
@@ -84,21 +106,34 @@ ws.on('message', function incoming(event) {
             send({ "cmd": "chat", "text": afkList[i] + " is AFK" });
         }
     }
+    //Generate a random room
+    if (msgR.startsWith("./channel") && !ignored.includes(msgNick)) {
+        send({ "cmd": "chat", "text": "?" + Math.random().toString(36).substr(2, 8) });
+    }
+    if (msgR.includes(msgFrom + " whispered: ./channel")) {
+        send({ "cmd": "whisper", "nick": msgFrom, "text": "?" + Math.random().toString(36).substr(2, 8) });
+    }
     //Check if kicked
-    if (msgR.includes(msgFrom + " whispered: ")) {
-        send({ "cmd": "whisper", "nick": msgFrom, "text": "Sent" });
+    if (msgR.includes(msgFrom + " whispered: kicked")) {
+        send({ "cmd": "whisper", "nick": msgFrom, "text": "Whisper recieved" });
     }
     //Show AFK users
     if (msgR == "./showafk" && msgTrip == "21YRcd") {
         send({ "cmd": "chat", "text": afkList.toString() });
     }
+    //Show ignored users
+    if (msgR == "./showignored" && msgTrip == "21YRcd") {
+        send({ "cmd": "chat", "text": ignored.toString() });
+    }
     //Ignore users
     if (msgR.startsWith("./ignore") && msgTrip == "21YRcd") {
         ignored.push(msgR.replace("./ignore ", ""));
+        console.log("Ignoring user " + msgR.replace("./ignore ", ""));
     }
     //Accept users
     if (msgR.startsWith("./accept") && msgTrip == "21YRcd") {
         ignored.splice(ignored.indexOf(msgR.replace("./accept ", ""), 1));
+        console.log("Accepting user " + msgR.replace("./accept ", ""));
     }
     //Show online users
     if (msgR.startsWith("./list") && !ignored.includes(msgNick)) {
@@ -110,7 +145,7 @@ ws.on('message', function incoming(event) {
     }
     //Remove user from online when they exit channel
     if (msgCmd == "onlineRemove") {
-        usersOnline = usersOnline.replace(msgNick, "");
+        usersOnline = usersOnline.replace("," + msgNick, "");
     }
     //Random number
     if (msgR.startsWith("./random") && !ignored.includes(msgNick)) {
@@ -120,6 +155,21 @@ ws.on('message', function incoming(event) {
         else {
             send({ "cmd": "chat", "text": Math.floor(Math.random() * msgR.replace("./random ", "") + 1).toString() });
         }
+    }
+    //Print color
+    if (msgR.startsWith("./printcolor") && !ignored.includes(msgNick)) {
+        if (msgR == "./printcolor") {
+            send({ "cmd": "chat", "text": "Enter a fucking hex value" });
+        }
+        else {
+            send({ "cmd": "chat", "text": "$\\def\\b{\\blacksquare\\\\}\\color{" + msgR.replace("./printcolor ", "") + "}{\\b}$" });
+        }
+    }
+    //Check when chat was last active
+    if (msgR.startsWith("./lastactive") && !ignored.includes(msgNick)) {
+        let lastChatMessage = (Date.now() - packetRecTime);
+        lastActiveTime = String((lastChatMessage/1000/60) + " Minutes");
+        send({ "cmd": "chat", "text": lastActiveTime + "\nLast recieved message: " + lastMessage });
     }
     //Check uptime
     if (msgR.startsWith("./uptime") & !ignored.includes(msgNick)) {
